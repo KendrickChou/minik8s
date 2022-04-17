975b0f0323cce98ac172939677f6ccbd260145a0
陈乐 <cen-le@sjtu.edu.cn>
2022-04-17 21:41:07 +0800

    feat: add some empty functions of ReplicaSet

---
+++ pkg/controller/replicaset/replicaset.go
@@ -0,0 +1,39 @@
+package replicaset
+
+type ReplicaSetController struct {
+}
+
+func NewReplicaSetController() *ReplicaSetController {
+	return &ReplicaSetController{}
+}
+
+// Run begins watching and syncing.
+func (rsc *ReplicaSetController) Run() {
+
+}
+
+func (rsc *ReplicaSetController) updateRS() {
+
+}
+
+func (rsc *ReplicaSetController) deleteRS() {
+
+}
+
+func (rsc *ReplicaSetController) manageReplicas() {
+
+}
+
+func (rsc *ReplicaSetController) getPods() {}
+
+func (rsc *ReplicaSetController) addPod() {
+
+}
+
+func (rsc *ReplicaSetController) updatePod() {
+
+}
+
+func (rsc *ReplicaSetController) deletePod() {
+
+}
