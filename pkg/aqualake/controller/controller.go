c94f0def8b086f5101d11df9b4ac71450a582ad2
kendrick <csc-0731@sjtu.edu.cn>
2022-06-01 23:00:18 +0800

    finish update function & action

--- pkg/aqualake/controller/controller.go
+++ pkg/aqualake/controller/controller.go
@@ -78,6 +78,33 @@ func SetUpRouter() *gin.Engine {
 		}
 	})
 
+	router.POST("/function/:id", func(ctx *gin.Context) {
+		funcId := ctx.Params.ByName("id")
+		bytes, err := couchdb.GetDoc(context.TODO(), constants.FunctionDBId, funcId)
+		if err != nil {
+			ctx.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		var f couchobject.Function
+		json.Unmarshal(bytes, &f)
+
+		buf, err := ioutil.ReadAll(ctx.Request.Body)
+
+		if err != nil {
+			ctx.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			return
+		}
+
+		err = couchdb.PutFile(context.TODO(), constants.FunctionDBId, funcId, funcId, f.Reversion, string(buf))
+
+		if err != nil {
+			ctx.JSON(http.StatusOK, gin.H{"error": err.Error()})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{"ok": true})
+		}
+	})
+
 	// Action Related
 	router.GET("/actionchain/:id", func(ctx *gin.Context) {
 		acId := ctx.Params.ByName("id")
